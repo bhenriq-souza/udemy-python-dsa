adb39eebe4abb9d56bf3e4d4cec4ea38396c2510
Bruno Henrique Souza <bhenriq.souza@gmail.com>
2025-07-29 07:40:30 -0300

    Implementing Linked List exercises

--- exercises/linked_list/find_middle_node.py
+++ exercises/linked_list/find_middle_node.py
@@ -6,13 +6,12 @@
 ## 1. You can only loop through the linked list once;
 ## 2. You are not allowed to calculate the length of the linked list;
 #####################################################################
-from common.node import Node
-from linked_list.linked_list import LinkedList
+from .types import ListNode
 
 
-def find_middle_node(linked_list: LinkedList) -> Node:
-    slow = linked_list.head
-    fast = linked_list.head
+def find_middle_node(head: ListNode) -> ListNode:
+    slow = head
+    fast = head
 
     while fast.next is not None:
         slow = slow.next
@@ -20,16 +19,16 @@ def find_middle_node(linked_list: LinkedList) -> Node:
 
         if fast and fast.next:
             fast = fast.next
-
+ 
     return slow
 
 def test_find_middle_node_even_elements_list():
     print("1 - Find middle node of list with even number of elements.")
 
-    linked_list = LinkedList(1)
-    linked_list.append(2)
-    linked_list.append(3)
-    linked_list.append(4)
+    linked_list = ListNode(1)
+    linked_list.next = ListNode(2)
+    linked_list.next.next = ListNode(3)
+    linked_list.next.next.next = ListNode(4)
     
     result = find_middle_node(linked_list)
     assert result.value == 3, f"Result {result.value} is not correct."
@@ -39,21 +38,23 @@ def test_find_middle_node_even_elements_list():
 def test_find_middle_node_odd_elements_list():
     print("2 - Find middle node of list with odd number of elements.")
     
-    linked_list = LinkedList(1)
-    linked_list.append(2)
-    linked_list.append(3)
-    linked_list.append(4)
-    linked_list.append(5)
+    linked_list = ListNode(1)
+    linked_list.next = ListNode(2)
+    linked_list.next.next = ListNode(3)
+    linked_list.next.next.next = ListNode(4)
+    linked_list.next.next.next.next = ListNode(5)
+    linked_list.next.next.next.next.next = ListNode(6)
+    linked_list.next.next.next.next.next.next = ListNode(7)
     
     result = find_middle_node(linked_list)
-    assert result.value == 3, f"Result {result.value} is not correct."
+    assert result.value == 4, f"Result {result.value} is not correct."
 
     print(f"Result {result.value} is correct.")
 
 def test_find_middle_node_single_node_list():
     print("3 - Find middle node of list with one element.")
 
-    linked_list = LinkedList(1)
+    linked_list = ListNode(1)
     result = find_middle_node(linked_list)
     assert result.value == 1, f"Result {result.value} is not correct."
 
